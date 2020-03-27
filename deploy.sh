0e0f7c20440fcefb4dcb3eacde41a5ee8c24a3f9
Felix Leupold <felixleupold90@gmail.com>
2020-03-27 16:39:56 +0100

    Adding autodeploy

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,14 @@ echo "Pushing to Docker-hub version $PACKAGE_VERSION, generated from branch $TRA
 docker build -t gnosispm/dex-price-estimator:$tag_name .;
 docker push gnosispm/dex-price-estimator:$tag_name;
 echo "The image has been pushed";
+
+if [ "$image_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
+    # Notifying webhook
+    curl -s  \
+      --output /dev/null \
+      --write-out "%{http_code}" \
+      -H "Content-Type: application/json" \
+      -X POST \
+      -d '{"push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
+      $AUTODEPLOY_URL
+fi
